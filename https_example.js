23ca4d2a6faf96d2c7135706b5a9086e4a00fd80
kwan14 <mendel.kwan@telus.net>
2017-09-10 15:27:57 +0000

    Sent stream from https object to stdout

--- https_example.js
+++ https_example.js
@@ -5,8 +5,9 @@ var options = {
   path : "/"
 };
 
-var callback = function() {
+var callback = function(response) {
   console.log("In response handler callback!");
+  console.log(response);
 }
 
 console.log("I'm about to make a request!");
